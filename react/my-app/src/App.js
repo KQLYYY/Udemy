55007f17cc753c395586b6baab87fbf0675a3658
kqlyyy <nikkar98@gmail.com>
2018-10-10 12:10:19 +0300

    initial commit

---
+++ react/my-app/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    // return (
+    //   <div className="App">
+    //     <h1>Hi, I'm a React App!</h1>
+    //   </div>
+    // );
+    return React.createElement('div', {className: 'Apps'}, React.createElement('h1',null,'Hi, I\'m a React App!'), 'Does this work now?');
+  }
+}
+
+export default App;
